8caba12a41a111c1c311a61e0cb75b2a561a5fcb
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-11-11 18:42:02 -0500

    added config and controllers folders

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017';
+
+connect(connectionString);
+
+module.exports = connection;
